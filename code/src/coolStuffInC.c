d5063e8e4fb265e9c7e2beb44728036b4e11fa1c
Deep Aggarwal <deep.uiuc@gmail.com>
2015-11-04 19:57:13 -0600

    Added src/functionPointersAdvanced.c

--- code/src/coolStuffInC.c
+++ code/src/coolStuffInC.c
@@ -3,8 +3,6 @@
 
 int main(int argc, char **argv)
 {
-    http://stackoverflow.com/questions/132241/hidden-features-of-c
-    http://stackoverflow.com/questions/599365/what-is-your-favorite-c-programming-trick
 
 
 }
--- code/src/functionPointers.c
+++ code/src/functionPointers.c
@@ -1,4 +1,7 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
 
 void func(int x)
 {
@@ -10,20 +13,19 @@ int addInt(int n, int m)
     return (n + m);
 }
 
-int main()
+int main(int argc, char **argv)
 {
     //Example1
-    void (*foo)(int);
-    foo = &func;
-    //Call func. NOTE: you can also use this syntax (*foo)(2)
-    foo(2);
-    (*foo)(2);
+    void (*foo)(int); //Declare a function pointer
+    foo = &func; //To initialize a function pointer, you must give it the address of a function in your program
+    foo(2); //To call the function pointed to by a function pointer, you treat the function pointer as though it were the name of the function you wish to call
+    (*foo)(2); //NOTE: you can also use this syntax. It is same as foo(2)
 
     //Example2
     int (*functionPtr)(int, int);
     functionPtr = &addInt;
     int sum1 = (*functionPtr)(2, 3); // sum1 == 5
-    int sum2 = functionPtr(2, 3);    // sum2 == 5
+    int sum2 = functionPtr(2, 3);    // sum2 == 5 with different syntax
     printf("%d\n", sum1);
     printf("%d\n", sum2);
 
